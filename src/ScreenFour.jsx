2ce1e785d3a0f4b3eb22aca67f22fac766e4d675
unknown <nguyenmanhtuancomputer@gmail.com>
2023-10-31 14:56:19 +0700

    lab

--- src/ScreenFour.jsx
+++ src/ScreenFour.jsx
@@ -1,10 +1,20 @@
 
-import { View } from "react-native"
+import { View, Image } from "react-native"
 const ScreenFour = ({route}) => {
-    const {data} = route
+    const {data} = route.params
     console.log(data)
     return (
-        <View></View>
+        <View>
+            <View style={{}}>
+                <Text>Order your</Text>
+            </View>
+            <View>
+                <Text>Order your</Text>
+            </View>
+            <View>
+                <Image style={{width: 150, height: 150}} source={{uri: data?.img}}></Image>
+            </View>
+        </View>
     )
 }
 
